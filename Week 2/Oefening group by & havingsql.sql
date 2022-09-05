2a43d671f2e5c6326d094bd13df3665c4009f3a9
Maurits Bakker <mauritsbakkerbreda@gmail.com>
2022-09-05 11:29:35 +0200

    Oefening group by & having na uitleg

--- Week 2/Oefening group by & havingsql.sql
+++ Week 2/Oefening group by & havingsql.sql
@@ -1,17 +1,35 @@
 -- Oefeningen GROUP BY & HAVING
 -- Geef de plaatsen en het aantal klanten per plaats
-SELECT PlaatsHfdkntr, COUNT(*) AS 'Aantal klanten'
+SELECT PlaatsHfdkntr, COUNT(PlaatsHfdkntr) AS 'Aantalklanten'
 FROM Klant
 GROUP BY PlaatsHfdkntr
 
 -- Geef de plaatsen met meer dan 1 klant
-SELECT PlaatsHfdkntr
+SELECT PlaatsHfdkntr, COUNT(PlaatsHfdkntr) AS 'Aantalklanten'
 FROM Klant
 GROUP BY PlaatsHfdkntr
-HAVING COUNT(*) > 1
+HAVING COUNT(PlaatsHfdkntr) > 1
 
 -- Geef de plaas(en) met meeste klanten
-SELECT PlaatsHfdkntr, COUNT(*) AS 'Aantal klanten'
+-- VIEW aanmaken
+CREATE VIEW AantalKlantenPerPlaats AS
+SELECT PlaatsHfdkntr, COUNT(PlaatsHfdkntr) AS 'Aantalklanten'
 FROM Klant
 GROUP BY PlaatsHfdkntr
-HAVING 
+
+--DROP VIEW AantalKlantenPerPlaats;
+
+SELECT MAX(AantalKlanten)
+FROM AantalKlantenPerPlaats
+
+SELECT PlaatsHfdkntr, COUNT(PlaatsHfdkntr) AS 'Aantalklanten'
+FROM Klant
+GROUP BY PlaatsHfdkntr
+HAVING COUNT(PlaatsHfdkntr) = (SELECT Aantalklanten 
+								FROM AantalKlantenPerPlaats)
+								
+-- snelle manier (niet goed)
+SELECT TOP 1 PlaatsHfdkntr, COUNT(PlaatsHfdkntr) AS 'Aantalklanten'
+FROM Klant
+GROUP BY PlaatsHfdkntr
+ORDER BY Aantalklanten DESC
